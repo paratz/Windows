71257f50a7f3597f0682f6e0f41b1286ace9cb4e
paratz <pablo@ratz.com.ar>
2017-03-31 10:35:51 -0300

    updat
    update

---
+++ Enable-SSLTrace.ps1
@@ -0,0 +1,2 @@
+﻿$NetCapFile = $env:SystemRoot + '\temp\netcap.etl'
+$StartNetCap = "netsh trace start traceFile=" + $NetCapFile  + " capture=yes maxsize=200 filemode=circular overwrite=yes report=no"
