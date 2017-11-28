c21faff5a48a8374b593e1382c1ae2460fdee985
paratz <pablo@ratz.com.ar>
2017-11-28 10:23:07 -0300

    uploading new stuff
    11]/28

---
+++ Ping-MultipleComputers.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+  [string]$ServerListFile
+  
+)
+
+#Este script sirve para hacer ping a múltiples computadoras.
+#
+#Utilización:
+#
+# .\Ping-MultipleComputers.ps1 -ServerListFile "C:\Temp\Computers.txt" >> Output.txt
+
+$ServerName = Get-Content $ServerListFile
+  
+foreach ($Server in $ServerName) {  
+  
+        if (test-Connection -ComputerName $Server -Count 2 -Quiet ) {   
+          
+            "$Server OK"  
+          
+                    } else  
+                      
+                    {"$Server Failed"  
+              
+                    }      
+          
+} 
--- Report-NeverExpiringPasswordLocalUsers.ps1
+++ Report-NeverExpiringPasswordLocalUsers.ps1
@@ -1,12 +1,18 @@
-﻿Param(
-  [string] $serverlist,
-  [string] $reporttextfile
-  )
+﻿
+Param( 
+  [string] $serverlist 
+#  [string] $reporttextfile 
+  ) 
 
-out-file -filepath $reporttextfile -append -InputObject ("Reporte Usuarios Locales " + (Get-Date))
+#out-file -filepath $reporttextfile -append -InputObject ("Reporte Usuarios Locales " + (Get-Date)) 
 
-foreach ($server in Get-Content $serverlist)
-{
-  "Reportando Servidor:" + $server | out-file -filepath $reporttextfile -append
-  Get-WmiObject -computername $Server -Class Win32_UserAccount -Filter  "LocalAccount='True'" | Where-Object {$_.PasswordExpires -eq "True"} | Select-Object Name, Status, Disabled, Lockout, PasswordRequired, PasswordChangeable, SID | out-file -filepath $reporttextfile -append
-  }
+foreach ($server in Get-Content $serverlist) 
+{ 
+
+$filename = $server + ".csv" 
+#  "Reportando Servidor:" + $server | out-file -filepath $reporttextfile -append 
+  Get-WmiObject -computername $Server -Class Win32_UserAccount -Filter  "LocalAccount='True'"  | Where-Object {$_.PasswordExpires -eq $False} | Select-Object Name, Status, Description, Disabled, Lockout, PasswordRequired, PasswordChangeable, PasswordExpires, SID | Export-Csv -Path $filename -NoTypeInformation -Encoding Unicode 
+# 
+
+  } 
+
